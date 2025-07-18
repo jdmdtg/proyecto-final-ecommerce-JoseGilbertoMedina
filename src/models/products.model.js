30f6f2f9aa7490a26ad8e9f78f7ce04f33f56a97
Jose Medina <146364099+jdmdtg@users.noreply.github.com>
2025-07-18 18:37:21 -0300

    sacamos FS

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -1,11 +1,11 @@
-import fs from "fs";
-import path from "path";
+// import fs from "fs";
+// import path from "path";
  
-const __dirname = import.meta.dirname;
+// const __dirname = import.meta.dirname;
 
-const jsonPath = path.join(__dirname, "./products.json");
-const json = fs.readFileSync(jsonPath, "utf-8");
-const products = JSON.parse(json);
+// const jsonPath = path.join(__dirname, "./products.json");
+// const json = fs.readFileSync(jsonPath, "utf-8");
+// const products = JSON.parse(json);
 
 // const Users_jsonPath = path.join(__dirname, "./users.json");
 // const User_json = fs.readFileSync(Users_jsonPath, "utf-8");
